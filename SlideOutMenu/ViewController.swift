bbcf6de7afa30b50ee3755f79022ecb4dd7fac9c
GaneshKumar Gunju <ganeshkumargunju@MacBook-Pro.local>
2018-06-01 10:36:37 +0530

    First commit

--- SlideOutMenu/ViewController.swift
+++ SlideOutMenu/ViewController.swift
@@ -9,12 +9,38 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var leadingC: NSLayoutConstraint!
+    @IBOutlet weak var tailingC: NSLayoutConstraint!
+    @IBOutlet weak var modView: UIView!
+    
+    
+    
+    var barButtonTappedIsVisible = false
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBAction func barButtonTapped(_ sender: Any) {
+        if !barButtonTappedIsVisible {
+            leadingC.constant = 150
+            tailingC.constant = -150
+            
+            barButtonTappedIsVisible = true
+        }
+        else{
+            leadingC.constant = 0
+            tailingC.constant = 0
+            
+            barButtonTappedIsVisible = false
+        }
+        UIView.animate(withDuration: 0.2, delay: 0.0, options:.curveEaseIn, animations: {
+            self.view.layoutIfNeeded()
+        }) { (animationCompleted) in
+            print("The animation is completed")
+        }
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
